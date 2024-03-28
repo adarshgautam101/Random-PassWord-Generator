256801f02ff9cd23f82c7aa74672f05325890d4b
Adarsh Gautam <109784927+adarshgautam101@users.noreply.github.com>
2024-03-28 21:46:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const passWordBox = document.getElementById("Password");
+const lenght = 12; // You can change length of the password 
+const upperCase = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+const lowerCase = "qwertyuioplkjhgfdsazxcvbnm";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{][<>/-=";
+const allChars = upperCase + lowerCase + number + symbol;
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+  while (lenght > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passWordBox.value = password;
+}
+
+function copyPassword(){
+    passWordBox.select();
+    document.execCommand("copy")
+}
